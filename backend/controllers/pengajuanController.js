8acc489c728d02cb60665405b0956440ac96f1df
aryaawcksn <aryaagun04@gmail.com>
2025-11-18 14:22:03 +0700

    Fixing more expresion on query

--- backend/controllers/pengajuanController.js
+++ backend/controllers/pengajuanController.js
@@ -89,12 +89,46 @@ export const createPengajuan = async (req, res) => {
     // Query SQL dengan kolom foto_ktp dan foto_selfie, plus field baru
     let query, values;
     if (hasStatusColumn) {
-      query = `
-        INSERT INTO pengajuan_tabungan 
-        (kode_referensi, nama_lengkap, nik, email, no_hp, tanggal_lahir, alamat, provinsi, kota, kode_pos, pekerjaan, penghasilan, cabang_pengambilan, jenis_kartu, status, foto_ktp, foto_selfie, jenis_kelamin, status_pernikahan, nama_ibu_kandung, kewarganegaraan, tempat_bekerja, alamat_kantor, sumber_dana, tujuan_rekening, jenis_rekening, kontak_darurat_nama, kontak_darurat_hp, setuju_data)
-        VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28)
-        RETURNING *;
-      `;
+     query = `
+  INSERT INTO pengajuan_tabungan 
+  (
+    kode_referensi,
+    nama_lengkap,
+    nik,
+    email,
+    no_hp,
+    tanggal_lahir,
+    alamat,
+    provinsi,
+    kota,
+    kode_pos,
+    pekerjaan,
+    penghasilan,
+    cabang_pengambilan,
+    jenis_kartu,
+    status,
+    foto_ktp,
+    foto_selfie,
+    jenis_kelamin,
+    status_pernikahan,
+    nama_ibu_kandung,
+    kewarganegaraan,
+    tempat_bekerja,
+    alamat_kantor,
+    sumber_dana,
+    tujuan_rekening,
+    jenis_rekening,
+    kontak_darurat_nama,
+    kontak_darurat_hp,
+    setuju_data
+  )
+  VALUES 
+  (
+    $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,
+    $15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29
+  )
+  RETURNING *;
+`;
       values = [
         kode_referensi,
         nama_lengkap,
