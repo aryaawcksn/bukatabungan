aaab6e97d249641f120c0b6984c38b2daabf50ec
aryaawcksn <aryaagun04@gmail.com>
2025-11-19 10:03:54 +0700

    Changing CJS to ESM on checknik

--- backend/routes/check-nik.js
+++ backend/routes/check-nik.js
@@ -1,19 +1,17 @@
-const express = require('express');
+import express from "express";
+import pool from "../config/db.js";
+
 const router = express.Router();
 
-router.get('/check-nik', async (req, res) => {
+router.get("/check-nik", async (req, res) => {
   const { nik } = req.query;
 
   const cek = await pool.query(
-    'SELECT id FROM pengajuan_tabungan WHERE nik = $1 LIMIT 1',
+    "SELECT id FROM pengajuan_tabungan WHERE nik = $1 LIMIT 1",
     [nik]
   );
 
-  if (cek.rows.length > 0) {
-    return res.json({ exists: true });
-  }
-
-  res.json({ exists: false });
+  res.json({ exists: cek.rows.length > 0 });
 });
 
-module.exports = router;
+export default router;
